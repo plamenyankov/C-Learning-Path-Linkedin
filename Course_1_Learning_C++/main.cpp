3f9c24f8d15296bd161e6dd6d44f3bf429662882
Plamen Yankov <p.s.yankov@abv.bg>
2022-02-23 10:31:36 +0200

    hello world

---
+++ Course_1_Learning_C++/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Plamen Yankov on 23.02.22.
+//
+
+#include "main.h"
+#include <iostream>
+
+int main(){
+
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
---
+++ Course_1_Learning_C++/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Plamen Yankov on 23.02.22.
+//
+
+#ifndef C_LEARNING_PATH_LINKEDIN_MAIN_H
+#define C_LEARNING_PATH_LINKEDIN_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //C_LEARNING_PATH_LINKEDIN_MAIN_H
