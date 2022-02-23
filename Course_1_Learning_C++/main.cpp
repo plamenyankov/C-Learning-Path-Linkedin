1e3cf30579a8eb1b784bad28ce5277c9c0abeb86
Plamen Yankov <p.s.yankov@abv.bg>
2022-02-23 12:15:10 +0200

    data types

--- Course_1_Learning_C++/main.cpp
+++ Course_1_Learning_C++/main.cpp
@@ -4,9 +4,24 @@
 
 #include "main.h"
 #include <iostream>
+#include <typeinfo>
+
+auto a = 1;
+auto b = 12312321312321;
+auto c = 3.14f;
+auto d = 3.14;
+auto e = true;
+auto f = 'b';
 
 int main(){
-
-    std::cout << "Hello World" << std::endl;
+//    std::string name;
+//    std::cout << "Write your name" << std::endl;
+//    std::cin >> name;
+    std::cout << typeid(a).name() << std::endl;
+    std::cout << typeid(b).name() << std::endl;
+    std::cout << typeid(c).name() << std::endl;
+    std::cout << typeid(d).name() << std::endl;
+    std::cout << typeid(e).name() << std::endl;
+    std::cout << typeid(f).name() << std::endl;
     return 0;
 }
