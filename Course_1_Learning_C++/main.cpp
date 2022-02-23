bf4df0c0750975213ec8cb966151ffdda9b515a6
Plamen Yankov <p.s.yankov@abv.bg>
2022-02-23 13:26:14 +0200

    arrays

--- Course_1_Learning_C++/main.cpp
+++ Course_1_Learning_C++/main.cpp
@@ -3,20 +3,17 @@
 //
 
 #include <iostream>
+
+
 #define HUNDRED 100
 #define DEBUG
-
-auto a = 1;
-auto b = 12312321312321;
-auto c = 3.14f;
-auto d = 3.14;
-auto e = true;
-auto f = 'b';
+char arr[23] = "hello guys";
+char arr2[] = "hello guys 2";
 
 int main(){
-    c += d;
-#ifdef DEBUG
-    std::cout << c << std::endl;
-#endif
+    strcat(arr,arr2);
+    for(auto i = 0; i < sizeof(arr); ++i){
+        std::cout << arr[i] << std::endl;
+    }
     return 0;
 }
