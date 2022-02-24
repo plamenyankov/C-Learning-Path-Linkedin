809935901e62ce7776037876f9a3af92fd066256
Plamen Yankov <p.s.yankov@abv.bg>
2022-02-24 14:52:45 +0200

    structures

--- Course_1_Learning_C++/main.cpp
+++ Course_1_Learning_C++/main.cpp
@@ -3,17 +3,20 @@
 //
 
 #include <iostream>
+enum car_purpose {ass, bad, girl};
 
-
-#define HUNDRED 100
-#define DEBUG
-char arr[23] = "hello guys";
-char arr2[] = "hello guys 2";
-
+struct Car{
+    std::string name;
+    int age;
+    unsigned char pur;
+};
 int main(){
-    strcat(arr,arr2);
-    for(auto i = 0; i < sizeof(arr); ++i){
-        std::cout << arr[i] << std::endl;
-    }
+//    Car car1;
+//    car1.name = "dad";
+//    car1.age = 12;
+//    car1.pur = bad;
+//
+//    std::cout << car1.name << std::endl;
+    std::cout << bad << std::endl;
     return 0;
 }
